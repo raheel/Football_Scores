6a0689841ccea06ff74adabcd85f63d4e3f012f7
raheel <raheel.khan@gmail.com>
2016-01-13 22:05:07 -0500

    resubmitting

--- app/src/main/java/barqsoft/footballscores/Utilities.java
+++ app/src/main/java/barqsoft/footballscores/Utilities.java
@@ -17,46 +17,47 @@ public class Utilities
     public static final int CHAMPIONS_LEAGUE = 362;
     public static final int PRIMERA_DIVISION = 358;
     public static final int BUNDESLIGA = 351;
-    public static String getLeague(int league_num)
+
+    public static String getLeague(Context context, int league_num)
     {
         switch (league_num)
         {
-            case SERIE_A : return "Seria A";
-            case PREMIER_LEGAUE : return "Premier League";
-            case CHAMPIONS_LEAGUE : return "UEFA Champions League";
-            case PRIMERA_DIVISION : return "Primera Division";
-            case BUNDESLIGA : return "Bundesliga";
-            default: return "Not known League Please report";
+            case SERIE_A : return context.getString(R.string.seriaa);
+            case PREMIER_LEGAUE : return context.getString(R.string.premierleague);
+            case CHAMPIONS_LEAGUE : return context.getString(R.string.champions_league);
+            case PRIMERA_DIVISION : return context.getString(R.string.primeradivison);
+            case BUNDESLIGA : return context.getString(R.string.bundesliga);
+            default: return context.getString(R.string.unknown_league);
         }
     }
-    public static String getMatchDay(int match_day,int league_num)
+    public static String getMatchDay(Context context, int match_day,int league_num)
     {
         if(league_num == CHAMPIONS_LEAGUE)
         {
             if (match_day <= 6)
             {
-                return "Group Stages, Matchday : 6";
+                return context.getString(R.string.matchday_text);
             }
             else if(match_day == 7 || match_day == 8)
             {
-                return "First Knockout round";
+                return context.getString(R.string.first_knockout_round);
             }
             else if(match_day == 9 || match_day == 10)
             {
-                return "QuarterFinal";
+                return context.getString(R.string.quarter_final);
             }
             else if(match_day == 11 || match_day == 12)
             {
-                return "SemiFinal";
+                return context.getString(R.string.semi_final);
             }
             else
             {
-                return "Final";
+                return context.getString(R.string.final_text);
             }
         }
         else
         {
-            return "Matchday : " + String.valueOf(match_day);
+            return context.getString(R.string.matchday_text) + " : " + String.valueOf(match_day);
         }
     }
 
@@ -72,25 +73,45 @@ public class Utilities
         }
     }
 
-    public static int getTeamCrestByTeamName (String teamname)
-    {
-        if (teamname==null){return R.drawable.no_icon;}
-        switch (teamname)
-        { //This is the set of icons that are currently in the app. Feel free to find and add more
-            //as you go.
-            case "Arsenal London FC" : return R.drawable.arsenal;
-            case "Manchester United FC" : return R.drawable.manchester_united;
-            case "Swansea City" : return R.drawable.swansea_city_afc;
-            case "Leicester City" : return R.drawable.leicester_city_fc_hd_logo;
-            case "Everton FC" : return R.drawable.everton_fc_logo1;
-            case "West Ham United FC" : return R.drawable.west_ham;
-            case "Tottenham Hotspur FC" : return R.drawable.tottenham_hotspur;
-            case "West Bromwich Albion" : return R.drawable.west_bromwich_albion_hd_logo;
-            case "Sunderland AFC" : return R.drawable.sunderland;
-            case "Stoke City FC" : return R.drawable.stoke_city;
-            default: return R.drawable.no_icon;
+    //changed to if statement instead of switch because using Strings from file for comparison
+    public static int getTeamCrestByTeamName(Context context, String teamname) {
+        if (teamname == null) {
+            return R.drawable.no_icon;
         }
+
+        if (teamname.equals(context.getString(R.string.arsenal)))
+            return R.drawable.arsenal;
+
+        if (teamname.equals(context.getString(R.string.manchester)))
+            return R.drawable.manchester_united;
+
+        if (teamname.equals(context.getString(R.string.swansea)))
+            return R.drawable.swansea_city_afc;
+
+        if (teamname.equals(context.getString(R.string.leicester)))
+            return R.drawable.leicester_city_fc_hd_logo;
+
+        if (teamname.equals(context.getString(R.string.everton)))
+            return R.drawable.everton_fc_logo1;
+
+        if (teamname.equals(context.getString(R.string.westham)))
+            return R.drawable.west_ham;
+
+        if (teamname.equals(context.getString(R.string.tottenham)))
+            return R.drawable.tottenham_hotspur;
+
+        if (teamname.equals(context.getString(R.string.westbromwich)))
+            return R.drawable.west_bromwich_albion_hd_logo;
+
+        if (teamname.equals(context.getString(R.string.sunderland)))
+            return R.drawable.sunderland;
+
+        if (teamname.equals(context.getString(R.string.stoke)))
+            return R.drawable.stoke_city;
+
+        return R.drawable.no_icon;
     }
+
 
     public static boolean isRTL() {
         return isRTL(Locale.getDefault());
